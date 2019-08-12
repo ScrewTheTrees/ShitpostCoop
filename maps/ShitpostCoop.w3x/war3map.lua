a87e96fd43c6d1571b6060b6a71c83913ad47985
ScrewTheTrees <fredrik@grimmenhag.se>
2019-08-12 20:53:38 +0200

    Push fixed shit

--- maps/ShitpostCoop.w3x/war3map.lua
+++ maps/ShitpostCoop.w3x/war3map.lua
@@ -1,50 +1,4 @@
-gg_trg_Melee_Initialization = nil
 function InitGlobals()
-end
-
-function CreateUnitsForPlayer0()
-    local p = Player(0)
-    local u
-    local unitID
-    local t
-    local life
-    u = CreateUnit(p, FourCC("hdhw"), -60.4, -1222.0, 112.459)
-end
-
-function CreatePlayerBuildings()
-end
-
-function CreatePlayerUnits()
-    CreateUnitsForPlayer0()
-end
-
-function CreateAllUnits()
-    CreatePlayerBuildings()
-    CreatePlayerUnits()
-end
-
-function Trig_Melee_Initialization_Actions()
-    MeleeStartingVisibility()
-    MeleeStartingHeroLimit()
-    MeleeGrantHeroItems()
-    MeleeStartingResources()
-    MeleeClearExcessUnits()
-    MeleeStartingUnits()
-    MeleeStartingAI()
-    MeleeInitVictoryDefeat()
-end
-
-function InitTrig_Melee_Initialization()
-    gg_trg_Melee_Initialization = CreateTrigger()
-    TriggerAddAction(gg_trg_Melee_Initialization, Trig_Melee_Initialization_Actions)
-end
-
-function InitCustomTriggers()
-    InitTrig_Melee_Initialization()
-end
-
-function RunInitializationTriggers()
-    ConditionalTriggerExecute(gg_trg_Melee_Initialization)
 end
 
 function InitCustomPlayerSlots()
@@ -66,11 +20,8 @@ function main()
     SetAmbientDaySound("LordaeronSummerDay")
     SetAmbientNightSound("LordaeronSummerNight")
     SetMapMusic("Music", true, 0)
-    CreateAllUnits()
     InitBlizzard()
     InitGlobals()
-    InitCustomTriggers()
-    RunInitializationTriggers()
 end
 
 function config()
@@ -79,7 +30,7 @@ function config()
     SetPlayers(1)
     SetTeams(1)
     SetGamePlacement(MAP_PLACEMENT_USE_MAP_SETTINGS)
-    DefineStartLocation(0, 5760.0, -4096.0)
+    DefineStartLocation(0, 128.0, -512.0)
     InitCustomPlayerSlots()
     SetPlayerSlotAvailable(Player(0), MAP_CONTROL_USER)
     InitGenericPlayerSlots()
