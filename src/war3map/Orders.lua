32810622c78135408794bc34627182e622b19e9d
ScrewTheTrees <fredrik@grimmenhag.se>
2019-08-13 22:21:30 +0200

    Add missing files

---
+++ src/war3map/Orders.lua
@@ -0,0 +1,9 @@
+local ____exports = {}
+____exports.Orders = {}
+____exports.Orders.move = "move"
+____exports.Orders.move = "move"
+____exports.Orders.attack = "attack"
+____exports.Orders.attack = "attack"
+____exports.Orders.smart = "smart"
+____exports.Orders.smart = "smart"
+return ____exports
